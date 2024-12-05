69af92f0d0adca8b20bd6d9fcd93af9db824290e
Aidan Bleser <aidanbleser35@gmail.com>
2024-12-05 15:09:12 -0600

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import "@fontsource/geist-mono"
+	import "@fontsource/geist-sans"
+	import { ModeWatcher } from "mode-watcher"
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<ModeWatcher/>
+{@render children()}
