ae5bee418f19e75b271b76ae500071ed8c36fe53
Aidan Bleser <aidanbleser35@gmail.com>
2024-12-10 11:04:26 -0600

    improves docs navigation with previous and next buttons

---
+++ src/lib/components/ui/prev-next/next.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { ChevronRight } from 'lucide-svelte';
+	import { cn } from '$lib/utils/utils';
+	import { Button, type ButtonProps } from '$lib/components/ui/button';
+
+	interface Props extends ButtonProps {
+		children: Snippet<[]>;
+	}
+
+	let { children, class: className, ...rest }: Props = $props();
+</script>
+
+<Button
+	class={cn('flex place-items-center gap-2 pl-4 pr-2', className)}
+	variant="outline"
+	size="sm"
+	{...rest}
+>
+	{@render children()}
+	<ChevronRight class="size-4" />
+</Button>
---
+++ src/lib/components/ui/prev-next/prev-next.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { cn } from '$lib/utils/utils';
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		previous?: Snippet<[]>;
+		next?: Snippet<[]>;
+		class?: string;
+	};
+
+	let { next, previous, class: className = undefined }: Props = $props();
+</script>
+
+<div class={cn('flex place-items-center justify-between', className)}>
+	<div>
+		{@render previous?.()}
+	</div>
+	<div>
+		{@render next?.()}
+	</div>
+</div>
