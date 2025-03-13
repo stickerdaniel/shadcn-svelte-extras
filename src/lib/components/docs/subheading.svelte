98d0807f678c25b55c61b3fd3b536fe1e27c7384
Aidan Bleser <aidanbleser35@gmail.com>
2025-03-13 07:24:41 -0500

    format

--- src/lib/components/docs/subheading.svelte
+++ src/lib/components/docs/subheading.svelte
@@ -10,13 +10,18 @@
 	let { class: className = undefined, children, ...rest }: Props = $props();
 
 	let ref = $state<HTMLHeadingElement>();
-	let id = $state<string>()
+	let id = $state<string>();
 
 	onMount(() => {
 		id = ref?.innerText.split(' ').join('-').toLowerCase();
-	})
+	});
 </script>
 
-<h2 bind:this={ref} {id} class={cn('mt-6 border-b pb-1 text-2xl font-semibold scroll-m-20', className)} {...rest}>
+<h2
+	bind:this={ref}
+	{id}
+	class={cn('mt-6 scroll-m-20 border-b pb-1 text-2xl font-semibold', className)}
+	{...rest}
+>
 	{@render children?.()}
 </h2>
