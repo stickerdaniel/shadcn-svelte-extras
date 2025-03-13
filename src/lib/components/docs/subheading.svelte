fd5f7ea80b51bf8a3eead71cddc0df9225c8276d
Aidan Bleser <aidanbleser35@gmail.com>
2025-03-13 07:20:24 -0500

    feat: Improvements to `ui/toc`

--- src/lib/components/docs/subheading.svelte
+++ src/lib/components/docs/subheading.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
 	import { cn } from '$lib/utils/utils';
+	import { onMount } from 'svelte';
 	import type { HTMLAttributes } from 'svelte/elements';
 
-	interface Props extends HTMLAttributes<HTMLHeadingElement> {
+	interface Props extends Omit<HTMLAttributes<HTMLHeadingElement>, 'id'> {
 		class?: string;
 	}
 
 	let { class: className = undefined, children, ...rest }: Props = $props();
+
+	let ref = $state<HTMLHeadingElement>();
+	let id = $state<string>()
+
+	onMount(() => {
+		id = ref?.innerText.split(' ').join('-').toLowerCase();
+	})
 </script>
 
-<h2 class={cn('mt-6 border-b pb-1 text-2xl font-semibold', className)} {...rest}>
+<h2 bind:this={ref} {id} class={cn('mt-6 border-b pb-1 text-2xl font-semibold scroll-m-20', className)} {...rest}>
 	{@render children?.()}
 </h2>
